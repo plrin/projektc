0c6d60443156c42a10b58e99a7bf983a5dab4c94
Duc Tan Dang <ductan.dang@googlemail.com>
2014-11-14 17:54:02 +0100

    removed trash, duplicate, etc

--- states/plugins/switchState.js
+++
@@ -1,6 +0,0 @@
-var myGame = new Kiwi.Game();
-myGame.states.addState(loadingState);
-myGame.states.addState(myState);
-myGame.states.addState(gameoverState);
-
-myGame.states.switchState("LoadingState");
